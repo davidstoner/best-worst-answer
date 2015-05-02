5041ffdfa455d194f52eaf3fe713619a064a23f9
David Stoner <david.stoner@aerialink.com>
2015-05-01 19:39:33 -0500

    remove Sails.js

--- assets/js/listeners.js
+++
@@ -1,3 +0,0 @@
-var answerMine = document.getElementById('answerMine');
-var submit = document.getElementById('submit');
-submit.addEventListener('click', function() {answerMine.className += ' transition';answerMine.className += ' slide-down';window.setTimeout(function() {answerMine.remove();}, 1000);}, false);
